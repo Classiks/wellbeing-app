d73d44a7f1cb3c6bb57284c16f4ce26011e06a61
Classiks <danielschulz1906@gmail.com>
2022-12-03 12:19:20 +0100

    update to singular

--- lib/providers_and_settings/showcase_texts.dart
+++ lib/providers_and_settings/showcase_texts.dart
@@ -2,13 +2,13 @@ const welcomeShowcaseText = ""
 "Welcome to the Wellbeing app.\n"
 "You can find all necessary Information in the Help-Tab.\n"
 "\n"
-"We recomment you create an account in the Settings-Tab,\nespecially when using the web version.";
+"Recommendation: Create an account in the Settings-Tab,\nespecially when using the web version.";
 
 
 const floatingActionButtonShowcaseText = ""
 "You can add new entries here.\n"
 "\n"
-"We recommend you first create some\nActivities in the Activity List Tab";
+"Recommendation: First create some\nActivities in the Activity List Tab";
 
 
 const userbuttonShowcaseText = ""
